b821047812b75990298f2705e7f36d15969c4b6b
namnthe150933 <namthanhnguyen601@gmail.com>
2023-09-23 19:51:25 +0700

    Create project

---
+++ BuildingManagementSystem/src/test/java/fpt/capstone/buildingmanagementsystem/BuildingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.capstone.buildingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
