954f6050ad508883e38adf0706d77a6c5aa38c12
namnthe150933 <namthanhnguyen601@gmail.com>
2023-09-27 21:29:45 +0700

    Update CorsConfig.java

--- BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/security/CorsConfig.java
+++ BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/security/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5137")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
