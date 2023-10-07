79cb324317ddf122e591a17e4caef54d2b0e38ab
namnthe150933 <namthanhnguyen601@gmail.com>
2023-10-07 14:56:44 +0700

    commit api livechat2 and refactor api get all message

--- BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/controller/LiveChatController.java
+++ BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/controller/LiveChatController.java
@@ -5,12 +5,10 @@ import fpt.capstone.buildingmanagementsystem.model.response.MessageResponse;
 import fpt.capstone.buildingmanagementsystem.service.LiveChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -25,7 +23,10 @@ public class LiveChatController {
     public ResponseEntity<?> createNewChat(@RequestBody ChatMessageRequest chatMessageRequest) {
         return liveChatService.createChat(chatMessageRequest);
     }
-
+    @RequestMapping(path = "/chat2", method = RequestMethod.POST, consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    public ResponseEntity<?> createNewChat2(@RequestParam("data") String data,@RequestParam("message") MultipartFile image) throws Exception {
+        return liveChatService.createChat2(data,image);
+    }
     @GetMapping("/message")
     public List<MessageResponse> getMessagesFromTo(@Param("from") String from, @Param("to") String to) {
         return liveChatService.getMessageBySenderAndReceiver(from, to);
