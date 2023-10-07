223f5ca0d0ef00245e780f6fbf085f3113688fe0
namnthe150933 <namthanhnguyen601@gmail.com>
2023-10-07 18:06:57 +0700

    Update ChatMessageRepository.java

--- BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/repository/ChatMessageRepository.java
+++ BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/repository/ChatMessageRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    @Query(value = "select * from chat_message where (sender_id = :from and receiver_id = :to) or (sender_id = :to and receiver_id = :from)", nativeQuery = true)
+    @Query(value = "select * from chat_message where (sender_id = :from and receiver_id = :to) or (sender_id = :to and receiver_id = :from) order by create_at ASC", nativeQuery = true)
     List<ChatMessage> findBySenderIdAndReceiverId(String from, String to);
 }
