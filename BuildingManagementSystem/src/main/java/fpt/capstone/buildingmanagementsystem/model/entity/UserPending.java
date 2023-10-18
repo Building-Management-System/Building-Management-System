fd4a72492f9b2573da38e59a941a4aa8d2c0c1ca
namnthe150933 <namthanhnguyen601@gmail.com>
2023-10-18 15:15:50 +0700

    refactor code

--- BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/model/entity/UserPending.java
+++ BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/model/entity/UserPending.java
@@ -3,7 +3,6 @@ package fpt.capstone.buildingmanagementsystem.model.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
