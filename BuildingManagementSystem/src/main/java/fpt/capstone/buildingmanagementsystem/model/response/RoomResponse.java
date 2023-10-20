346744a2004f3838ccb2be3301afe41ef68d2eb5
phamsangvd <105622184+phamsangvd@users.noreply.github.com>
2023-10-20 22:15:45 +0700

    get RoomService

---
+++ BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/model/response/RoomResponse.java
@@ -0,0 +1,12 @@
+package fpt.capstone.buildingmanagementsystem.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RoomResponse {
+    private int roomId;
+
+    private String roomName;
+}
