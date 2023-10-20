9d6285f1de8686dd47fe1a004b6577e793defcae
tungnshe161905 <tungnshe161905@fpt.edu.vn>
2023-10-20 23:13:10 +0700

    commit account

--- BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/BuildingManagementSystemApplication.java
+++ BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/BuildingManagementSystemApplication.java
@@ -22,7 +22,7 @@ public class BuildingManagementSystemApplication {
         InitializationService initDB = context.getBean(InitializationService.class);
         initDB.init();
     }
-    //note
+    //note 1
     @PostConstruct
     public void init(){
         // Setting Spring Boot SetTimeZone
