f0a0c24c4c9dceeb0eb898801e81967696cff3f4
namnthe150933 <namthanhnguyen601@gmail.com>
2023-10-26 21:19:38 +0700

    refactoer code api accept room request

--- BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/model/request/RoomBookingRequest.java
+++ BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/model/request/RoomBookingRequest.java
@@ -1,10 +1,12 @@
 package fpt.capstone.buildingmanagementsystem.model.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class RoomBookingRequest {
     String roomBookingFormRoomId;
 
--- BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/repository/RoomBookingFormRoomRepository.java
+++ BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/repository/RoomBookingFormRoomRepository.java
@@ -1,8 +1,11 @@
 package fpt.capstone.buildingmanagementsystem.repository;
 
+import fpt.capstone.buildingmanagementsystem.model.dto.RoomBookingDTO;
+import fpt.capstone.buildingmanagementsystem.model.entity.Room;
 import fpt.capstone.buildingmanagementsystem.model.entity.RoomBookingFormRoom;
 import fpt.capstone.buildingmanagementsystem.model.entity.requestForm.RoomBookingRequestForm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +13,7 @@ import java.util.List;
 @Repository
 public interface RoomBookingFormRoomRepository extends JpaRepository<RoomBookingFormRoom, String> {
     List<RoomBookingFormRoom> findByRoomRequestFormIn(List<RoomBookingRequestForm> roomBookingRequestForms);
+    RoomBookingFormRoom findByRoomRequestForm(RoomBookingRequestForm roomBookingRequestForm);
+    List<RoomBookingFormRoom> findByRoomRequestFormInAndRoom(List<RoomBookingRequestForm> roomBookingRequestForms, Room room);
 
 }
