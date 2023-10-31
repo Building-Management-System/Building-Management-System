3fc7ac746ea4cac9b1d5efb8c49fb9050608d131
tungnshe161905 <tungnshe161905@fpt.edu.vn>
2023-11-01 00:10:36 +0700

    commit HolidayService

--- BuildingManagementSystem/src/test/java/fpt/capstone/buildingmanagementsystem/service/HolidayServiceTest.java
+++ BuildingManagementSystem/src/test/java/fpt/capstone/buildingmanagementsystem/service/HolidayServiceTest.java
@@ -104,5 +104,3 @@ class HolidayServiceTest {
         Assertions.assertEquals(true, result);
     }
 }
-
-//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
