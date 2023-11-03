64907e9addc8cbab2baaaa334b468a715e655c98
phamsangvd <105622184+phamsangvd@users.noreply.github.com>
2023-11-04 00:36:59 +0700

    test

--- BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/BuildingManagementSystemApplication.java
+++ BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/BuildingManagementSystemApplication.java
@@ -4,9 +4,10 @@ import fpt.capstone.buildingmanagementsystem.service.InitializationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableScheduling
+@EnableScheduling
 public class BuildingManagementSystemApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(BuildingManagementSystemApplication.class);
