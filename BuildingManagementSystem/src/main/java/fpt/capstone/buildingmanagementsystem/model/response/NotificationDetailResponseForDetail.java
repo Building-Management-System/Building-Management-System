03d07850d27ad47d4fc6c36b692bad62b14e5192
phamsangvd <105622184+phamsangvd@users.noreply.github.com>
2023-11-06 00:40:26 +0700

    change userName

--- BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/model/response/NotificationDetailResponseForDetail.java
+++ BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/model/response/NotificationDetailResponseForDetail.java
@@ -25,8 +25,7 @@ public class NotificationDetailResponseForDetail {
     private NotificationStatus notificationStatus;
     private boolean priority;
     private String creatorId;
-    private String creatorFirstName;
-    private String creatorLastName;
+    private String userName;
     private Department creatorDepartment;
     private String creatorImage;
     private boolean personalPriority;
