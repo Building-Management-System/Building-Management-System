e19d9b1ca950a2c1dd36b114d846296c2a635f70
namnthe150933 <namthanhnguyen601@gmail.com>
2023-11-21 10:58:18 +0700

    commit api get change log detail

---
+++ BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/model/response/ChangeLogDetailResponse.java
@@ -0,0 +1,28 @@
+package fpt.capstone.buildingmanagementsystem.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.util.List;
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Builder
+public class ChangeLogDetailResponse {
+    private String name;
+    private String username;
+    private String departmentName;
+    private String dateDaily;
+    private Time checkin;
+    private Time checkout;
+    private Time checkinChange;
+    private Time checkoutChange;
+    private String dateDailyChange;
+    private String changeFrom;
+    private boolean violate;
+    private double outSideWork;
+    List<ControlLogResponse> controlLogResponse;
+}
--- BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/service/RequestChangeLogService.java
+++ BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/service/RequestChangeLogService.java
@@ -2,30 +2,46 @@ package fpt.capstone.buildingmanagementsystem.service;
 
 import fpt.capstone.buildingmanagementsystem.exception.BadRequest;
 import fpt.capstone.buildingmanagementsystem.exception.Conflict;
+import fpt.capstone.buildingmanagementsystem.exception.NotFound;
 import fpt.capstone.buildingmanagementsystem.exception.ServerError;
 import fpt.capstone.buildingmanagementsystem.mapper.ChangeLogMapper;
 import fpt.capstone.buildingmanagementsystem.model.entity.ChangeLog;
+import fpt.capstone.buildingmanagementsystem.model.entity.ControlLogLcd;
+import fpt.capstone.buildingmanagementsystem.model.entity.DailyLog;
 import fpt.capstone.buildingmanagementsystem.model.entity.User;
 import fpt.capstone.buildingmanagementsystem.model.request.SaveChangeLogRequest;
+import fpt.capstone.buildingmanagementsystem.model.response.AttendanceDetailResponse;
+import fpt.capstone.buildingmanagementsystem.model.response.ChangeLogDetailResponse;
+import fpt.capstone.buildingmanagementsystem.model.response.ControlLogResponse;
 import fpt.capstone.buildingmanagementsystem.repository.ChangeLogRepository;
+import fpt.capstone.buildingmanagementsystem.repository.ControlLogLcdRepository;
+import fpt.capstone.buildingmanagementsystem.repository.DailyLogRepository;
 import fpt.capstone.buildingmanagementsystem.repository.UserRepository;
+import fpt.capstone.buildingmanagementsystem.until.Until;
+import fpt.capstone.buildingmanagementsystem.validate.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Time;
 import java.text.ParseException;
-import java.util.Optional;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static fpt.capstone.buildingmanagementsystem.validate.Validate.validateStartTimeAndEndTime;
 
 @Service
 public class RequestChangeLogService {
     @Autowired
+    ControlLogLcdRepository controlLogLcdRepository;
+    @Autowired
+    DailyLogRepository dailyLogRepository;
+    @Autowired
     UserRepository userRepository;
     @Autowired
     ChangeLogRepository changeLogRepository;
     @Autowired
     ChangeLogMapper changeLogMapper;
-
     public boolean saveChangeLog(SaveChangeLogRequest saveChangeLogRequest) {
         try {
             if (saveChangeLogRequest.getManagerId() != null &&
@@ -41,8 +57,15 @@ public class RequestChangeLogService {
                         if (saveChangeLogRequest.getWorkOutSide() == null) changeLog.setOutsideWork(-1);
                         changeLog.setEmployee(userEmp.get());
                         changeLog.setManager(userManager.get());
-                        changeLogRepository.save(changeLog);
-                        return true;
+                        Optional<ChangeLog> optionalChangeLog=changeLogRepository.getChangeLogDetailByUserIdAndDate(saveChangeLogRequest.getEmployeeId(),saveChangeLogRequest.getDate());
+                        if(optionalChangeLog.isPresent()) {
+                            changeLog.setChangeLogId(optionalChangeLog.get().getChangeLogId());
+                            changeLogRepository.save(changeLog);
+                            return true;
+                        }else {
+                            changeLogRepository.save(changeLog);
+                            return true;
+                        }
                     } else {
                         throw new BadRequest("request_fail");
                     }
@@ -54,6 +77,61 @@ public class RequestChangeLogService {
             }
         } catch (ServerError | ParseException e) {
             throw new ServerError("fail");
+        }
+    }
+    public ChangeLogDetailResponse getChangeLogDetail(String employee_id, String date) {
+        if (employee_id != null && Validate.validateDateFormat(date)) {
+            try {
+                Optional<DailyLog> dailyLogOptional = dailyLogRepository.getAttendanceDetailByUserIdAndDate(employee_id, date);
+                if (dailyLogOptional.isPresent()) {
+                    DailyLog dailyLogs = dailyLogOptional.get();
+                    String name = dailyLogs.getUser().getFirstName() + " " + dailyLogs.getUser().getLastName();
+                    String username = dailyLogs.getUser().getAccount().getUsername();
+                    List<ControlLogLcd> controlLogLcds = controlLogLcdRepository.getControlLogLcdList(username, date);
+                    controlLogLcds=controlLogLcds.stream()
+                            .sorted((Comparator.comparing(ControlLogLcd::getTime)))
+                            .collect(Collectors.toList());
+                    String departmentName = dailyLogs.getUser().getDepartment().getDepartmentName();
+                    SimpleDateFormat sdf = new SimpleDateFormat("EEEE, MMMM dd, yyyy", Locale.US);
+                    String dateDaily = sdf.format(Until.convertDateToCalender(dailyLogs.getDate()).getTime());
+                    Time checkin = dailyLogs.getCheckin();
+                    Time checkout = dailyLogs.getCheckout();
+                    Optional<ChangeLog> changeLog=changeLogRepository.getChangeLogDetailByUserIdAndDate(employee_id,date);
+                    Time checkinChange = null;
+                    Time checkoutChange = null;
+                    String dateDailyChange = null;
+                    String changeFrom = null;
+                    boolean violate = false;
+                    double outSideWork = 0;
+                    if(changeLog.isPresent()) {
+                         checkinChange = changeLog.get().getCheckin();
+                         checkoutChange = changeLog.get().getCheckout();
+                         dateDailyChange = sdf.format(Until.convertDateToCalender(changeLog.get().getCreatedDate()).getTime());
+                         changeFrom = changeLog.get().getManager().getAccount().getUsername();
+                         violate = changeLog.get().isViolate();
+                         outSideWork = changeLog.get().getOutsideWork();
+                    }
+                    List<ControlLogResponse> controlLogResponse = new ArrayList<>();
+                    controlLogLcds.forEach(element -> {
+                        ControlLogResponse controlLogResponse1 = new ControlLogResponse();
+                        controlLogResponse1.setLog(element.getTime().toString());
+                        controlLogResponse1.setUsername(username);
+                        controlLogResponse.add(controlLogResponse1);
+                    });
+                    return ChangeLogDetailResponse.builder()
+                            .name(name).username(username).departmentName(departmentName).dateDaily(dateDaily)
+                            .checkin(checkin).checkout(checkout).checkinChange(checkinChange).checkoutChange(checkoutChange).dateDailyChange(dateDailyChange)
+                            .violate(violate).changeFrom(changeFrom).outSideWork(outSideWork)
+                            .controlLogResponse(controlLogResponse)
+                            .build();
+                } else {
+                    throw new NotFound("request_fail");
+                }
+            } catch (ServerError | ParseException e) {
+                throw new ServerError("fail");
+            }
+        } else {
+            throw new BadRequest("request_fail");
         }
     }
 }
