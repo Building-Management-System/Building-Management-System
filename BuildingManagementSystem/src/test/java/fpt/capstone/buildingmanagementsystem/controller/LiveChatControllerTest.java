cad3515dc43a0e917654d66202aa365af84e533e
tungnshe161905 <tungnshe161905@fpt.edu.vn>
2023-12-07 22:32:10 +0700

    Test LiveChatControllerTest

---
+++ BuildingManagementSystem/src/test/java/fpt/capstone/buildingmanagementsystem/controller/LiveChatControllerTest.java
@@ -0,0 +1,128 @@
+package fpt.capstone.buildingmanagementsystem.controller;
+
+import fpt.capstone.buildingmanagementsystem.model.request.*;
+import fpt.capstone.buildingmanagementsystem.model.response.*;
+import fpt.capstone.buildingmanagementsystem.service.LiveChatService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+class LiveChatControllerTest {
+    @Mock
+    LiveChatService liveChatService;
+    @InjectMocks
+    LiveChatController liveChatController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testCreateNewChat() {
+        when(liveChatService.createChat(any())).thenReturn(new ListChatResponse("chatId", "chatName", "isGroupChat", List.of("String"), List.of(new UserInfoResponse("accountId", "username", "firstName", "lastName", "image", "roleName")), new GregorianCalendar(2023, Calendar.DECEMBER, 7, 22, 25).getTime(), "read", "admin"));
+
+        ListChatResponse result = liveChatController.createNewChat(new CreateChatRequest("from", "chatName", List.of("String"), "message"));
+        Assertions.assertEquals(new ListChatResponse("chatId", "chatName", "isGroupChat", List.of("String"), List.of(new UserInfoResponse("accountId", "username", "firstName", "lastName", "image", "roleName")), new GregorianCalendar(2023, Calendar.DECEMBER, 7, 22, 25).getTime(), "read", "admin"), result);
+    }
+
+    @Test
+    void testCreateNewMessage() {
+        when(liveChatService.newChatMessage(any())).thenReturn(new MessageImageAndFileResponse("messageId", "message", "senderId"));
+
+        MessageImageAndFileResponse result = liveChatController.createNewMessage(new ChatMessageRequest("from", "chatId", "message"));
+        Assertions.assertEquals(new MessageImageAndFileResponse("messageId", "message", "senderId"), result);
+    }
+
+    @Test
+    void testCreateNewMessage2() throws IOException {
+        when(liveChatService.newChatMessage2(anyString(), any())).thenReturn(new MessageImageAndFileResponse("messageId", "message", "senderId"));
+
+        MessageImageAndFileResponse result = liveChatController.createNewMessage2("data", null);
+        Assertions.assertEquals(new MessageImageAndFileResponse("messageId", "message", "senderId"), result);
+    }
+
+    @Test
+    void testCreateNewMessage3() throws IOException {
+        when(liveChatService.newChatMessage3(anyString(), any())).thenReturn(new MessageImageAndFileResponse("messageId", "message", "senderId"));
+
+        MessageImageAndFileResponse result = liveChatController.createNewMessage3("data", null);
+        Assertions.assertEquals(new MessageImageAndFileResponse("messageId", "message", "senderId"), result);
+    }
+
+    @Test
+    void testGetMessagesByChatId() {
+        when(liveChatService.getMessageBySenderAndReceiver(anyString(), anyString())).thenReturn(new ChatResponse(List.of(new MessageResponse("messageId", true, "message", "senderId", "createdAt", "type")), List.of(new UserChatResponse("userId", "username", "image")), "admin"));
+
+        ChatResponse result = liveChatController.getMessagesByChatId("chatId", "userId");
+        Assertions.assertEquals(new ChatResponse(List.of(new MessageResponse("messageId", true, "message", "senderId", "createdAt", "type")), List.of(new UserChatResponse("userId", "username", "image")), "admin"), result);
+    }
+
+    @Test
+    void testGetAllChatUserSingle() {
+        when(liveChatService.getAllChatUserSingle(anyString())).thenReturn(List.of(new UserInfoResponse("accountId", "username", "firstName", "lastName", "image", "roleName")));
+
+        List<UserInfoResponse> result = liveChatController.getAllChatUserSingle(new GetUserInfoRequest("userId"));
+        Assertions.assertEquals(List.of(new UserInfoResponse("accountId", "username", "firstName", "lastName", "image", "roleName")), result);
+    }
+
+    @Test
+    void testGetAllChat() {
+        when(liveChatService.getAllChat(anyString())).thenReturn(List.of(new ListChatResponse("chatId", "chatName", "isGroupChat", List.of("String"), List.of(new UserInfoResponse("accountId", "username", "firstName", "lastName", "image", "roleName")), new GregorianCalendar(2023, Calendar.DECEMBER, 7, 22, 25).getTime(), "read", "admin")));
+
+        List<ListChatResponse> result = liveChatController.getAllChat(new GetUserInfoRequest("userId"));
+        Assertions.assertEquals(List.of(new ListChatResponse("chatId", "chatName", "isGroupChat", List.of("String"), List.of(new UserInfoResponse("accountId", "username", "firstName", "lastName", "image", "roleName")), new GregorianCalendar(2023, Calendar.DECEMBER, 7, 22, 25).getTime(), "read", "admin")), result);
+    }
+
+    @Test
+    void testUpdateChat() {
+        when(liveChatService.updateChat(any())).thenReturn(new ListChatResponse("chatId", "chatName", "isGroupChat", List.of("String"), List.of(new UserInfoResponse("accountId", "username", "firstName", "lastName", "image", "roleName")), new GregorianCalendar(2023, Calendar.DECEMBER, 7, 22, 25).getTime(), "read", "admin"));
+
+        ListChatResponse result = liveChatController.updateChat(new UpdateGroupChatRequest("chatId", "isGroup", "chatName", List.of("String")));
+        Assertions.assertEquals(new ListChatResponse("chatId", "chatName", "isGroupChat", List.of("String"), List.of(new UserInfoResponse("accountId", "username", "firstName", "lastName", "image", "roleName")), new GregorianCalendar(2023, Calendar.DECEMBER, 7, 22, 25).getTime(), "read", "admin"), result);
+    }
+
+    @Test
+    void testRemoveFromChat() {
+        when(liveChatService.removeFromChat(any())).thenReturn(true);
+
+        boolean result = liveChatController.removeFromChat(new RemoveUserAndChangeAdminRequest("chatId", "userId"));
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    void testReadChat() {
+        when(liveChatService.readChat(any())).thenReturn(true);
+
+        boolean result = liveChatController.readChat(new RemoveUserAndChangeAdminRequest("chatId", "userId"));
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    void testUpdateChange() {
+        when(liveChatService.updateChange(any())).thenReturn(true);
+
+        boolean result = liveChatController.updateChange(new RemoveUserAndChangeAdminRequest("chatId", "userId"));
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    void testGetFileChatDownload() {
+        when(liveChatService.getFileChatDownload(any())).thenReturn(new FileDataResponse("fileId", "name", "type", new byte[]{(byte) 0}));
+
+        FileDataResponse result = liveChatController.getFileChatDownload(new FileRequest("messageId", "fileName"));
+        Assertions.assertEquals(new FileDataResponse("fileId", "name", "type", new byte[]{(byte) 0}), result);
+    }
+}
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
