c8fb22462444822d24803fb19d4394346dff8a6c
namnthe150933 <namthanhnguyen601@gmail.com>
2023-12-10 20:22:00 +0700

    push api export change log detail

--- BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/controller/ChangeLogController.java
+++ BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/controller/ChangeLogController.java
@@ -4,6 +4,7 @@ import fpt.capstone.buildingmanagementsystem.model.request.ChangeLogRequest;
 import fpt.capstone.buildingmanagementsystem.model.request.SaveChangeLogRequest;
 import fpt.capstone.buildingmanagementsystem.model.response.ChangeLogDetailResponse;
 import fpt.capstone.buildingmanagementsystem.model.response.ChangeLogResponse;
+import fpt.capstone.buildingmanagementsystem.service.ChangeLogReportDetailPDFService;
 import fpt.capstone.buildingmanagementsystem.service.ChangeLogService;
 import fpt.capstone.buildingmanagementsystem.service.RequestChangeLogService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -23,6 +25,8 @@ public class ChangeLogController {
     @Autowired
     RequestChangeLogService requestChangeLogService;
 
+    @Autowired
+    ChangeLogReportDetailPDFService changeLogReportDetailPDFService;
     @Autowired
     ChangeLogService changeLogService;
 
@@ -44,5 +48,9 @@ public class ChangeLogController {
     @GetMapping("/getChangeLogDetail")
     public ChangeLogDetailResponse getChangeLogDetail(@Param("change_log_id") String change_log_id,@Param("employee_id") String employee_id, @Param("date") String date) {
         return requestChangeLogService.getChangeLogDetail(change_log_id,employee_id, date);
+    }
+    @GetMapping("/exportChangeLogReportDetail")
+    public ResponseEntity<byte[]> exportChangeLogReportDetail(@Param("change_log_id") String change_log_id,@Param("employee_id") String employee_id, @Param("date") String date) throws IOException {
+        return changeLogReportDetailPDFService.export(change_log_id,employee_id, date);
     }
 }
--- BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/service/ChangeLogReportDetailPDFService.java
+++ BuildingManagementSystem/src/main/java/fpt/capstone/buildingmanagementsystem/service/ChangeLogReportDetailPDFService.java
@@ -2,27 +2,110 @@ package fpt.capstone.buildingmanagementsystem.service;
 
 import com.lowagie.text.*;
 import com.lowagie.text.Font;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
-import fpt.capstone.buildingmanagementsystem.model.request.MonthlyEvaluateRequest;
-import fpt.capstone.buildingmanagementsystem.model.response.MonthlyEvaluateResponse;
+import fpt.capstone.buildingmanagementsystem.model.response.ChangeLogDetailResponse;
+import fpt.capstone.buildingmanagementsystem.model.response.ControlLogResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import java.awt.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
-
-import static fpt.capstone.buildingmanagementsystem.model.enumEnitty.EvaluateEnum.*;
 
 @Service
 public class ChangeLogReportDetailPDFService {
+    @Autowired
+    RequestChangeLogService requestChangeLogService;
+    private void writeTableHeader(PdfPTable table) {
+        PdfPCell cell = new PdfPCell();
+        cell.setBackgroundColor(Color.getHSBColor(-0.5f,0.542f,0.525f));
+        cell.setPadding(5);
+        com.lowagie.text.Font font = FontFactory.getFont(FontFactory.HELVETICA);
+        font.setColor(Color.WHITE);
+        cell.setPhrase(new Phrase("  User Name", font));
+        table.addCell(cell);
+        cell.setPhrase(new Phrase("  Date Time", font));
+        table.addCell(cell);
+    }
 
+    public ResponseEntity<byte[]> export(String changeLogId, String userId, String date) throws DocumentException, IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, byteArrayOutputStream);
+        ChangeLogDetailResponse changeLogDetailResponse=requestChangeLogService.getChangeLogDetail(changeLogId,userId,date);
+        document.open();
 
+        Font font = FontFactory.getFont(FontFactory.TIMES_ROMAN);
+        font.setSize(23);
+        font.setColor(Color.getHSBColor(0.5f,0.542f,0.525f));
+
+        Font font2 = FontFactory.getFont(FontFactory.TIMES_ROMAN);
+        font2.setSize(18);
+        font2.setColor(Color.DARK_GRAY);
+
+        Paragraph p = new Paragraph("Change Log Report Detail", font);
+        p.setAlignment(Paragraph.ALIGN_CENTER);
+        document.topMargin();
+        document.add(p);
+
+        Paragraph info = new Paragraph("1. Information", font2);
+        info.setAlignment(Paragraph.ALIGN_LEFT);
+        document.add(info);
+        document.add(new Paragraph("    Employee: "+changeLogDetailResponse.getName()));
+        document.add(new Paragraph("    Account: "+changeLogDetailResponse.getUsername()));
+        document.add(new Paragraph("    Department: "+changeLogDetailResponse.getDepartmentName()));
+
+        Paragraph systemLog = new Paragraph("2. System Log", font2);
+        systemLog.setAlignment(Paragraph.ALIGN_LEFT);
+        document.add(systemLog);
+        document.add(new Paragraph("    Date: "+changeLogDetailResponse.getDateDaily()));
+        document.add(new Paragraph("    Check In: "+changeLogDetailResponse.getCheckin()));
+        document.add(new Paragraph("    Check Out: "+changeLogDetailResponse.getCheckout()));
+
+        Paragraph attendanceChange = new Paragraph("3. Attendance Change", font2);
+        attendanceChange.setAlignment(Paragraph.ALIGN_LEFT);
+        document.add(attendanceChange);
+        document.add(new Paragraph("    Check In Change: "+changeLogDetailResponse.getCheckinChange()));
+        document.add(new Paragraph("    Check Out Change: "+changeLogDetailResponse.getCheckoutChange()));
+        document.add(new Paragraph("    Date Change: "+changeLogDetailResponse.getDateDailyChange()));
+        document.add(new Paragraph("    Change from: "+changeLogDetailResponse.getChangeFrom()));
+
+        Paragraph log = new Paragraph("4. Log", font2);
+        log.setAlignment(Paragraph.ALIGN_LEFT);
+        document.add(log);
+        document.add(new Paragraph("    Outside Work: "+changeLogDetailResponse.getOutSideWork()));
+        document.add(new Paragraph("    Violate: "+changeLogDetailResponse.isViolate()));
+
+        Paragraph reason = new Paragraph("5. Reason", font2);
+        reason.setAlignment(Paragraph.ALIGN_LEFT);
+        document.add(reason);
+        document.add(new Paragraph("    +) "+changeLogDetailResponse.getReason()));
+
+        Paragraph controlLog = new Paragraph("6. Control Log", font2);
+        document.add(controlLog);
+        PdfPTable table = new PdfPTable(2);
+        table.setWidthPercentage(100f);
+        table.setWidths(new float[] {3.5f, 5f});
+        table.setSpacingBefore(10);
+        writeTableHeader(table);
+        for (ControlLogResponse controlLogResponse : changeLogDetailResponse.getControlLogResponse()) {
+            table.addCell("  "+controlLogResponse.getUsername());
+            table.addCell("  "+controlLogResponse.getLog());
+        }
+        document.add(table);
+        document.close();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDispositionFormData("attachment", "ChangeLogReportDetail_"+changeLogDetailResponse.getUsername()+"_"+date+".pdf");
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(byteArrayOutputStream.size())
+                .body(byteArrayOutputStream.toByteArray());
+    }
 
 }
